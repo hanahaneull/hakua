0000000361236e9da692727fb098dcff4a06a040
Haneul Seong <hana@disroot.org>
2021-10-31 19:32:53 +0700

    Add delete message method

--- discord/message.js
+++ discord/message.js
@@ -2,4 +2,13 @@ module.exports = class Message {
   constructor (client) {
     this.http = client.http.client
   }
+
+  delete(channelID, messageID) {
+    return new Promise(async (resolve, reject) => {
+      if (!channelID) reject('Invalid channel ID')
+      if (!messageID) reject('Invalid message ID')
+      const res = this.http.delete(`channels/${channelID}/messages/${messageID}`).catch(reject)
+      resolve(res)
+    })
+  }
 }
